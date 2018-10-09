c8e26fc96022890bcb8ebc95d43a7681a726ea0b
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-10-09 14:45:25 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,6 +1,20 @@
+
 def count_elements(array)
   new_hash = Hash.new(0)
   array.each do |item| new_hash[item] +=1
   end
   new_hash
 end
+
+
+# def count_elements(array)
+#   new_hash = {}
+#   array.each do |item|
+#     if new_hash[item] != nil
+#       new_hash[item] += 1
+#     else
+#       new_hash[item] = 1
+#     end
+#   end
+#   new_hash
+# end
